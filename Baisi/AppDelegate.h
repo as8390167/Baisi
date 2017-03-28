ae731682ab7c1800593685738ddd94f7cec65507
元 <yuan.wang@dragonflow.com>
2017-03-28 17:00:59 +0800

    Initial Commit

---
+++ Baisi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Baisi
+//
+//  Created by SW on 2017/3/28.
+//  Copyright © 2017年 WY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Baisi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Baisi
+//
+//  Created by SW on 2017/3/28.
+//  Copyright © 2017年 WY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
